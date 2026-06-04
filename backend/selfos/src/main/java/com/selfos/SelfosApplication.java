7d950e46f73d0aba2eec798a539deb475f02d031
Rajesh-C01 <rajeshchakali01@gmail.com>
2026-06-04 09:10:47 +0000

    Phase 2 completed - Spring Boot and PostgreSQL setup

---
+++ backend/selfos/src/main/java/com/selfos/SelfosApplication.java
@@ -0,0 +1,13 @@
+package com.selfos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfosApplication.class, args);
+	}
+
+}
---
+++ backend/selfos/src/test/java/com/selfos/SelfosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
